4cccbd09fe9996649eabb293a62805519ede5f15
Atharv Date <atharvdate5114@gmail.com>
2026-04-27 19:50:28 +0530

    Added PrintSubArrays program

---
+++ src/arrays/PrintSubArrays.java
@@ -0,0 +1,22 @@
+package arrays;
+public class PrintSubArrays {
+    public static void subArrays(int[] arr){
+        for(int i=0; i< arr.length; i++) {
+            int start = i;
+            for (int j = i; j<arr.length; j++) {
+                int end = j;
+                for (int k = start; k<=end; k++) {
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args){
+        int[] arr = {1,-2,6,-1,3};
+        subArrays(arr);
+    }
+}
