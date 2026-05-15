0b64debeb5ab96d627e05a5c26e84b9e0ec73435
Atharv Date <atharvdate5114@gmail.com>
2026-05-15 17:04:23 +0530

    Applied bubble sort on an integer array, tc= O(n)

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,4 +1,38 @@
+//this program is modular, as u can see throughout the program
 package sorting;
 
 public class BubbleSort {
+
+    public static void sort(int[] nums){
+        for(int turn=0; turn< nums.length-1; turn++){
+            boolean swapped = false;
+            for(int j=0; j<=nums.length-2-turn; j++){
+                if(nums[j]>nums[j+1]){
+                    // swap them
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+
+                    swapped =true;
+                }
+            }
+            //if no swapping happened, that means our array was already sorted
+            if(!swapped){
+                break;
+            }
+        }
+    }
+    //lets print our sorted array here using new fresh method
+    public static void printSort(int[] nums){
+        for(int i=0; i<nums.length; i++) {
+            System.out.print(nums[i]+" ");
+        }
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,2,5,6,9,8,7,1,4};
+
+        sort(nums);
+        printSort(nums);
+    }
 }
