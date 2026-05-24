4b4e9f2e9f85ad57672d67b879494adf6bf6611c
Atharv Date <atharvdate5114@gmail.com>
2026-05-24 20:46:22 +0530

    Solved LeetCode Q.1470 (Easy) with TC=O(n) and SC=O(n) using array indexing and shuffle approach

---
+++ src/leetcodequiz/dsa/ShuffleArrays.java
@@ -0,0 +1,23 @@
+package leetcodequiz.dsa;
+import java.util.Arrays;
+
+public class ShuffleArrays {
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        int index = 0;
+        for(int i=0; i<n; i++){
+            ans[index] = nums[i];
+            ans[index+1] = nums[i+n];
+            index +=2;
+        }
+        return ans;
+    }
+
+    public static void main(String[] ans){
+        int[] nums = {1,2,3,4,4,3,2,1};
+        int n = 4;
+
+        //call
+        System.out.println(Arrays.toString(shuffle(nums, n)));
+    }
+}
