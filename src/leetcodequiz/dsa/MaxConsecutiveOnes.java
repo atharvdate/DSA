f1e240a5211cc014ea3da9419b1ef66d9bd09d96
Atharv Date <atharvdate5114@gmail.com>
2026-05-24 20:42:37 +0530

    Solved LeetCode Q.485 (Easy) with TC=O(n) and SC=O(1) using optimal traversal approach

---
+++ src/leetcodequiz/dsa/MaxConsecutiveOnes.java
@@ -0,0 +1,26 @@
+package leetcodequiz.dsa;
+public class MaxConsecutiveOnes {
+    public static int maxConsecutive(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int max =0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==1){
+                count++;
+                if(count>max){
+                    max =count;
+                }
+            }else{
+                count =0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,0,1,1,0,1,1,1,0,1};
+
+        System.out.println(maxConsecutive(nums));
+    }
+}
