384a68a4e089f8dbe284b926731b899b26799c83
Atharv Date <atharvdate5114@gmail.com>
2026-06-01 13:22:28 +0530

    Solved lowercase vowel count using string traversal with TC=O(n) and SC=O(1)

---
+++ src/strings/LowercaseVowelCount.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class LowercaseVowelCount {
+    public static int countOccurrence(String str){
+        int count = 0;
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        String str= "hello WORLD by Riya";
+
+        //call function here
+        System.out.println(countOccurrence(str));
+    }
+}
