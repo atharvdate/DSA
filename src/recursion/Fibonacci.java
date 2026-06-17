0c98c9217fe140af6480dac4273544554645db54
Atharv Date <atharvdate5114@gmail.com>
2026-06-17 19:49:07 +0530

    Fibonacci using recursion

---
+++ src/recursion/Fibonacci.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class Fibonacci {
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+
+        int fibnm1 = fib(n-1);
+        int fibnm2 = fib(n-2);
+        int fibn = fibnm1+fibnm2;
+        return fibn;
+    }
+
+    public static void main(String[] args){
+        int n = 7;
+        System.out.println(fib(n));
+    }
+}
