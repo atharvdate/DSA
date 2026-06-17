e02745e70db3d6d23011b0d7ef20f05ee7bea319
Atharv Date <atharvdate5114@gmail.com>
2026-06-17 18:43:57 +0530

    Sum of natural numbers

---
+++ src/recursion/SumOfNaturalNums.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class SumOfNaturalNums {
+    public static int sum(int n){
+        if(n==1){
+            return 1;
+        }
+
+        int Snm1 = sum(n-1);
+        int Sn = n+Snm1;
+        return Sn;
+    }
+
+
+    public static void main(String[] args){
+        int n = 5;
+
+        System.out.println(sum(n));
+    }
+}
