d309046f575e6a8bcd79a39d5a89eb20b2fd32c2
Atharv Date <atharvdate5114@gmail.com>
2026-07-26 22:27:11 +0530

    Solved LeetCode Q.162(Medium) Find Peak Element, TC=O(n) and SC=O(1)

---
+++ src/arrays/FindPeakEle.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class FindPeakEle {
+    public static int findPeak(int[] nums){
+        int n = nums.length;
+        if(n==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        for(int i=1; i<n-1; i++){
+            if(nums[i-1]<nums[i] && nums[i]> nums[i+1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] nums = {1,2,1,3,5,6,4};
+        System.out.println(findPeak(nums));
+    }
+}
